8c126f771e991dd73c52b689be9b6268f60d601e
Syed Abbas Ali <syedabbas83778@gmail.com>
2022-11-28 20:37:15 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var form=document.querySelector("form");
+var fname=document.getElementById("fname");
+var lname=document.getElementById("lname");
+var email=document.getElementById("email");
+var pass=document.getElementById("password");
+
+form.addEventListener("submit",()=>{
+    
+})
